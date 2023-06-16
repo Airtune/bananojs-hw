6a03a28d6ef3339b518a8885f79061c70fefc8e0
Airtune <airtune@protonmail.com>
2023-06-16 17:56:46 -0300

    remove console.log from TransportU2F.js

--- webpack/TransportU2F.js
+++ webpack/TransportU2F.js
@@ -1,5 +1,3 @@
 
-console.log(`TransportU2F LOAD!`);
 const TransportU2F = require('@ledgerhq/hw-transport-u2f').default;
-console.log(`TransportU2F: ${TransportU2F}`);
 global.window.TransportU2F = TransportU2F;
